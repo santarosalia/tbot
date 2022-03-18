48fd521048fd1280ce1fd787328906758b03402d
dope <mdc@kakao.com>
2022-03-19 00:04:33 +0900

    a

--- notion.js
+++ notion.js
@@ -1,83 +1,120 @@
-const { Client } = require("@notionhq/client")
+import { Client } from "@notionhq/client"
 
-const notion = new Client({
-    auth: process.env.NOTION_TOKEN,
-})
+const notion = new Client({ auth: process.env.NOTION_KEY })
 
-const allThings = async ()  => {
-    try {
-        const items = await notion.databases.query({
-            database_id: process.env.NOTION_DATABASE_ID,
-        })
+const databaseId = process.env.NOTION_DATABASE_ID
 
-        return items.results.map((list)=>{
-            const properties = JSON.parse(JSON.stringify(list.properties))
-            return properties.name.title[0].text.content
-        }).reduce((prev, curr)=> `${prev}\n${curr}`)
-    } catch (e) {
-        console.log(e)
-    }
-}
-
-const leftDateFive = async () => {
-    try {
-        const items = await notion.databases.query({
-            database_id: process.env.NOTION_DATABASE_ID,
-            filter: {
-                property: "left",
-                formula: {
-                    number: {
-                        less_than : 5
-                    },
-                },
-            },
-        })
-        return items.results.map((list)=>{
-            const properties = JSON.parse(JSON.stringify(list.properties))
-            return {
-                name: properties.name.title[0].text.content,
-                left: properties.left.formula.number
+async function addItem(text) {
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        title: { 
+          title:[
+            {
+              "text": {
+                "content": text
+              }
             }
-        })
-    } catch (error) {
-        console.log(error)
-    }
+          ]
+        }
+      },
+    })
+    console.log(response)
+    console.log("Success! Entry added.")
+  } catch (error) {
+    console.error(error.body)
+  }
 }
 
-const getTypeList = async () => {
-    try {
-        const items = await notion.databases.query({
-            database_id: process.env.NOTION_DATABASE_ID,
-        })
-        const result = items.results.map((row)=>{
-            const properties = JSON.parse(JSON.stringify(row.properties))
-            return properties.type.select.name
-        })
-        return Array.from(new Set(result));
-    } catch (e) {
-        console.log(e)
-    }
-}
+addItem("Yurts in Big Sur, California")
 
-const typeFilter = async (name) => {
-    try {
-        const items = await notion.databases.query({
-            database_id: process.env.NOTION_DATABASE_ID,
-            filter: {
-                property: "type",
-                select: {
-                    equals: name,
-                },
-            },
-        })
-        return items.results.map((row)=>{
-            const properties = JSON.parse(JSON.stringify(row.properties))
-            return properties.name.title[0].text.content
-        }).reduce((prev, curr)=> `${prev}\n${curr}`)
 
-    } catch (e) {
-        console.log('typeFilter', e)
-    }
-}
 
-module.exports = {typeFilter, getTypeList, leftDateFive, allThings}
+
+
+
+
+// const { Client } = require("@notionhq/client")
+
+// const notion = new Client({
+//     auth: process.env.NOTION_TOKEN,
+// })
+
+// const allThings = async ()  => {
+//     try {
+//         const items = await notion.databases.query({
+//             database_id: process.env.NOTION_DATABASE_ID,
+//         })
+
+//         return items.results.map((list)=>{
+//             const properties = JSON.parse(JSON.stringify(list.properties))
+//             return properties.name.title[0].text.content
+//         }).reduce((prev, curr)=> `${prev}\n${curr}`)
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
+
+// const leftDateFive = async () => {
+//     try {
+//         const items = await notion.databases.query({
+//             database_id: process.env.NOTION_DATABASE_ID,
+//             filter: {
+//                 property: "left",
+//                 formula: {
+//                     number: {
+//                         less_than : 5
+//                     },
+//                 },
+//             },
+//         })
+//         return items.results.map((list)=>{
+//             const properties = JSON.parse(JSON.stringify(list.properties))
+//             return {
+//                 name: properties.name.title[0].text.content,
+//                 left: properties.left.formula.number
+//             }
+//         })
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// const getTypeList = async () => {
+//     try {
+//         const items = await notion.databases.query({
+//             database_id: process.env.NOTION_DATABASE_ID,
+//         })
+//         const result = items.results.map((row)=>{
+//             const properties = JSON.parse(JSON.stringify(row.properties))
+//             return properties.type.select.name
+//         })
+//         return Array.from(new Set(result));
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
+
+// const typeFilter = async (name) => {
+//     try {
+//         const items = await notion.databases.query({
+//             database_id: process.env.NOTION_DATABASE_ID,
+//             filter: {
+//                 property: "type",
+//                 select: {
+//                     equals: name,
+//                 },
+//             },
+//         })
+//         return items.results.map((row)=>{
+//             const properties = JSON.parse(JSON.stringify(row.properties))
+//             return properties.name.title[0].text.content
+//         }).reduce((prev, curr)=> `${prev}\n${curr}`)
+
+//     } catch (e) {
+//         console.log('typeFilter', e)
+//     }
+// }
+
+// module.exports = {typeFilter, getTypeList, leftDateFive, allThings}
