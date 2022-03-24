e1badd2f8854295c54eaf609df4fc10638afee91
dope <mdc@kakao.com>
2022-03-24 23:36:13 +0900

    a

--- poll.js
+++ poll.js
@@ -1,3 +1,3 @@
 async function a(a,t,e=(()=>!1)){do{if(await a(),await e())break;const i="number"==typeof t?t:t();await new Promise((a=>setTimeout(a,Math.max(0,i))))}while(!await e())}
-export{a as poll};
+
 module.exports = a;
