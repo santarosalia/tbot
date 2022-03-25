478792c2fbad5aaf52d3c25614214223b76c204b
dope <mdc@kakao.com>
2022-03-25 14:18:30 +0900

    a

--- bot.js
+++ bot.js
@@ -80,8 +80,8 @@ const options = {
         const timePast = info[4].candle_date_time_kst;
         const onePer = tradePrice5/10000;
         
-        const plus = tradePrice1-tradePrice5;
-        const minus = tradePrice5-tradePrice1;
+        const minus = tradePrice1-tradePrice5;
+        const plus = tradePrice5-tradePrice1;
         
         if((tradePrice5-tradePrice1) >=onePer){
           bot.sendMessage('5133524983',timePast.split('T')[1]+' 기준'+'\n'+market+'의 가격 : '+tradePrice5+' => '+tradePrice1+'\n'+'0.01% 이상 하락 : '+plus+'원').then();
