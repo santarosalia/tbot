329c42b631b58a24201a54bbcf2a2bce82b83da3
dope <mdc@kakao.com>
2022-03-26 09:43:51 +0900

    a

--- bot.js
+++ bot.js
@@ -12,37 +12,41 @@ const { poll } = require('./poll');
 let bot;
 
 if(process.env.NODE_ENV === 'production') {
-  bot = new Bot(token);
-  bot.setWebHook(process.env.HEROKU_URL + bot.token);
+    bot = new Bot(token);
+    bot.setWebHook(process.env.HEROKU_URL + bot.token);
 }
 else {
-  bot = new Bot(token, { polling: true });
+    bot = new Bot(token, { polling: true });
 }
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
 bot.on('message',async (msg) => {
-  console.log(msg.from.first_name+'의 메세지 :'+msg.text);
-  
-  const name = msg.from.first_name;
-  if(msg.text=='id'){
+    console.log(msg.from.first_name+'의 메세지 :'+msg.text);
+
+    const name = msg.from.first_name;
+    if(msg.text=='id'){
     console.log(msg.chat.id);
     
     
     
-  }
-  if(msg.text=='전체보기'){
+    }
+    if(msg.text=='전체보기'){
     bot.sendMessage(msg.chat.id,'',{
-      "reply_markup" : {
+    "reply_markup" : {
         "keyboard" : [["BTC-???"],["BTC-KRW"]]
 
-      }
+    }
     }).then();
     
 
-  }
-  
+    }
 
+
+});
+bot.onText(/\/start/,async (msg) =>{
+    const chatId =msg.chat.id;
+    bot.sendMessage(chatId,'hello '+msg.chat.first_name);
 });
 
 
@@ -114,25 +118,7 @@ const options = {
 
     
     
-    // request(options, function (error, response, body) {
-    //   if (error) throw new Error(error);
-    //   const info = JSON.parse(body);
-      
 
-    //   for(i in info){
-        
-    //     const market = info[i].market;
-    //     const koreanName = info[i].korean_name;
-    //     const englishName = info[i].english_name;
-    //     const marketWarning = info[i].market_warning;
-        
-        
-        
-        
-    //     createSomething(market,koreanName,englishName,marketWarning).then();
-        
-    //   }
-    // });
 
 
 module.exports = bot;
