7e28d1890690f9dd8f046a2522ce3d0883522f1d
dope <mdc@kakao.com>
2022-03-29 00:16:44 +0900

    a

--- bot.js
+++ bot.js
@@ -115,7 +115,7 @@ bot.onText(/\/myRegist/,async(msg)=>{
   
   
   const result2 = await myRegist(chatId.toString());
-  console.log('result2 1: '+result2[]);
+  console.log('result2 1: '+result2);
   console.log('result2 2: '+result2.then());
   
   
