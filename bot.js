7a0ff0cfe747aee0cefcf69b371c852672a7edc3
dope <mdc@kakao.com>
2022-03-28 14:53:32 +0900

    a

--- bot.js
+++ bot.js
@@ -95,7 +95,7 @@ bot.sendMessage(chatId,warningList);
 });
 
 });
-bot.onText(/^\/add\sKRW-[A-Z]{3}$/,async(msg)=>{
+bot.onText(/^\/add\sKRW-\w+/,async(msg)=>{
   const chatId =msg.chat.id;
   bot.sendMessage(chatId,'list add');
 });
